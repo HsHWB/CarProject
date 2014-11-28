decdd042f7dc9784057f94d196b2dd0e451b98f1
HsHWB <Hswbinforgithub@gmail.com>
2014-11-28 17:12:19 +0800

    一分钟显示一次时间，调整布局

--- src/com/example/pad/MainActivity.java
+++ src/com/example/pad/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.pad;
 /**
  * 平板端，小区停车场activity
  */
+import java.text.SimpleDateFormat;
+
+import java.util.Timer;
+import java.util.TimerTask;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	public static int screenWidth;
+	private String date = null;//时间更新
+	TextView tvTime;
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		screenWidth  = getWindowManager().getDefaultDisplay().getWidth();
 		setContentView(R.layout.activity_main);
-		
+	    tvTime = (TextView)findViewById(R.id.tvTime);//时间textview
+		setTimer.schedule(timeTask, 0, 60000);
 	}
 
 	@Override
@@ -37,4 +47,30 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	/**
+	 * 更新时间
+	 */
+	private final Timer setTimer = new Timer();
+	private TimerTask timeTask = new TimerTask() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			Message msg = new Message();
+			msg.what = 0x12a;
+			setTimeHandler.sendMessage(msg);
+		}
+	};
+	
+	private Handler setTimeHandler = new Handler(){
+		@Override
+		public void handleMessage(Message msg){
+			if(msg.what == 0x12a){
+				SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy.MM.dd hh:mm");       
+				date = sDateFormat.format(new java.util.Date()); 
+				tvTime.setText(date);
+			}
+		}
+	};
 }
